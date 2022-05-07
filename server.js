7d8f55a7a60995e39cfbfae36af9ef1bb9d20145
AndreaCuriel <acurielipn@hotmail.com>
2022-05-06 20:17:13 -0500

    Método Delete para eliminar un mission commander

--- server.js
+++ server.js
@@ -149,6 +149,12 @@ app.put("/missionCommanders/:id", async (req, res) => {
   return res.json({ message: "Actualizado correctamente" });
 });
 
+app.delete("/missionCommanders/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  await prisma.missionCommander.delete({ where: { id: id } });
+  return res.json({ message: "Eliminado correctamente" });
+});
+
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
 });
